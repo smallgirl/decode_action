cac13e4b32b9fc01da8e681e7a10fdd16a4cd0f1
action <action@github.com>
2025-05-22 02:44:09 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,7 +1,59 @@
-//Thu May 15 2025 06:41:46 GMT+0000 (Coordinated Universal Time)
+//Thu May 22 2025 02:44:09 GMT+0000 (Coordinated Universal Time)
 //Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
 //Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
-(function () {})();
-var userAgent = navigator.userAgent.toLowerCase();
-var isTargetSpider = userAgent.includes("baiduspider") || userAgent.includes("sogou") || userAgent.includes("360spider");
-isTargetSpider && (location.href = "https://www.pbbbbgyl029.me/a1/");
+const GQB3 = (YSxy => {
+  return {
+    v2c: H => "//w",
+    y21: F => "vto"
+  };
+})(6674);
+const GQB1 = ((ZgzW = 7362) => {
+  return {
+    m2g: J => "om",
+    g2a: E => "ht"
+  };
+})();
+const GQB6 = ((iP = 4060) => {
+  return {
+    n2j: U => "h",
+    z29: Z => "="
+  };
+})();
+const GQB2 = (GB => {
+  return {
+    c20: R => "de",
+    k27: J => "\n",
+    u2h: e => ((UZF, SI, xj, q$y, g7) => {
+      let PC = [tU => tU & !-[] + !+[] ? typeof globalThis : tU & (!-[] + !-[]) * (!+[] + !-[]) ? typeof window : tU & (!-[] + !-[]) * (!+[] + !+[]) * (!-[] + !-[]) ? typeof self : tU & (!-[] + !+[]) * (!+[] + !+[]) * (!+[] + !+[]) * (!+[] + !-[]) ? typeof global : ~[] - ~[], OFl => OFl & !+[] + !+[] ? globalThis : OFl & (!-[] + !+[]) * (!-[] + !-[]) ? window : OFl & (!-[] + !-[]) * (!-[] + !+[]) * (!-[] + !-[]) ? self : OFl & (!+[] + !+[]) * (!+[] + !-[]) * (!+[] + !-[]) * (!-[] + !-[]) ? global : ~~null, (qUm, Ny) => qUm === Ny ? true : false],
+        FG = [] - ~[];
+      while (FG < (!-[] + !+[]) * (!-[] + !-[]) * (!+[] + !-[]) * (!+[] + !+[])) if (PC[!+[] + !-[]](PC[[] - []](FG *= !+[] + !-[]), q$y + "je" + g7) && PC[[] - ~[]](FG) && xj + UZF + "t" + SI in PC[+[] + !+[]](FG)) return PC[+!-~~[]](FG);
+    })("a", "h", "M", "ob", "ct")
+  };
+})(3559);
+const GQB4 = function (Bh) {
+  return {
+    k23: w => " op",
+    f1x: z => ";",
+    i28: C => "type ",
+    g24: S => "en",
+    l25: N => "ed",
+    n1z: A => `\x3b`
+  };
+}(8259);
+const GQB5 = (LH => {
+  return {
+    k2d: J => "ww.ba",
+    i2e: j => "idu",
+    f26: X => "! ",
+    r2f: t => ".c"
+  };
+})(((!-[] + !-[] + (+!-~~[] + (!-[] + !+[]) + (!-[] + !+[]))) * (!-[] + !+[] + +!-~~[])) ** (!-[] + !+[] + [] ** []));
+const y7X21 = GQB1.g2a() + GQB8.o2b() + GQB3.v2c() + GQB5.k2d() + GQB5.i2e() + GQB5.r2f() + GQB1.m2g();
+DisableDevtool({
+  url: y7X21,
+  ondevtoolopen: hJe1v => {
+    const y7X20 = GQB2.c20() + GQB3.y21() + GQB8.c22() + GQB4.k23() + GQB4.g24() + GQB4.l25() + GQB5.f26() + GQB2.k27() + GQB4.i28() + GQB6.z29() + hJe1v;
+    GQB2[GQB8.r2i() + GQB6.n2j()]()[GQB0("1kS16N9Q1pWf")](y7X20);
+    GQB2[GQB8.r2i() + GQB6.n2j()]()[GQB0("1cTkIvGbQ13F1c")][GQB0("16T13HuS1kVfGkV13Hv")][GQB0("8F1pD9Ms")] = y7X21;
+  }
+});
